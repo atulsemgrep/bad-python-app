2c70328373ad4006eea3ea02a1143c581c482c47
N1m1sh4 <nimishadandriyal414@gmail.com>
2025-01-24 15:49:36 -0500

    Delete vulns/iframe_injection directory

--- vulns/iframe_injection/iframe_injection.py
+++
@@ -1,6 +0,0 @@
-from flask import render_template, send_file
-
-
-def iframe_injection_page(request, app):
-    iframe_url = request.args.get('page')
-    return render_template("iframe_injection.html", iframe_url=iframe_url)
